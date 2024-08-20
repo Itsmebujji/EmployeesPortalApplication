f5ac02dc9eac4d3f1fd1a4a0b82837fe629af375
Vineethkumar <vineetkumar568326@gmail.com>
2024-08-20 23:02:24 +0530

    Initial Changes Done

---
+++ src/main/java/com/vineeth/epa/employeesportalapplication/EmployeesPortalApplication.java
@@ -0,0 +1,14 @@
+package com.vineeth.epa.employeesportalapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeesPortalApplication.class, args);
+        System.out.println("Hi Vineeth");
+    }
+
+}
---
+++ src/test/java/com/vineeth/epa/employeesportalapplication/EmployeesPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vineeth.epa.employeesportalapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
