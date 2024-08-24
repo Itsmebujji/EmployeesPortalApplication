71969075f9c5914fd962d2f570379f300831d8c9
sreeram7777 <sreeramkumar777@gmail.com>
2024-08-24 09:14:27 +0530

    New Changes udpated

--- src/main/java/com/vineeth/epa/employeesportalapplication/EmployeesPortalApplication.java
+++ src/main/java/com/vineeth/epa/employeesportalapplication/EmployeesPortalApplication.java
@@ -1,14 +1,38 @@
 package com.vineeth.epa.employeesportalapplication;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@Component
 public class EmployeesPortalApplication {
+
+    @Value("${server.port}")
+    public int appName;
+
+    public static int staticValue;
+
+    /*
+       @PostConstruct - why we use this annotation?
+       -> it is
+    */
+
+    @PostConstruct
+    public void init() {
+    staticValue = appName;
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(EmployeesPortalApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeesPortalApplication.class, args);
-        System.out.println("Hi Vineeth");
+        logger.info("Application started");
+        System.out.println("port"+staticValue);
     }
 
 }
